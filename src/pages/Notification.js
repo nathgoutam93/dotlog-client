486d2b5d5e61cb0f8d4341b04b6dbaecb2930754
nathgoutam93 <nathgoutam93@gmail.com>
2022-01-05 19:11:38 +0530

    bug fix

--- src/pages/Notification.js
+++ src/pages/Notification.js
@@ -1,5 +1,4 @@
-import React from 'react';
-import { useEffect } from 'react/cjs/react.development';
+import React, { useEffect } from 'react';
 import { useHeader } from '../context/headerContext';
 import { useNavbar } from '../context/navbarContext';
 
